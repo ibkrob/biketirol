11a6f8735964c1586ac4b83954caafe5e97a7441
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-06-20 23:11:36 +0200

    korrektur data

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ overlays.gpx.addTo(map);
 
 
 // GPX Track Layer implementieren
-let gpxTrack = new L.GPX("../data/gps-daten-etappe-14-westendorf-alpbach.gpx", {
+let gpxTrack = new L.GPX("./data/gps-daten-etappe-14-westendorf-alpbach.gpx", {
 async: true,
 marker_options:{
     startIconUrl:'icons/start.png',
